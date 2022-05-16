6be83d1cc1dab1b646823cceade9f16f72d5d1e9
Giuseppe <giuseppecantafio@hotmail.it>
2022-05-16 11:53:10 +0200

    scaffolding + librerie fontawesome e bootstrap

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./style/general.scss";
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-center">Scaffolding <i class="fa-solid fa-meteor"></i></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
